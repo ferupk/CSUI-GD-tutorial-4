c403b9c092f1981d98c00592dbdb858ce24325f4
ferupk <feru.pratama@ui.ac.id>
2025-03-07 18:07:55 +0700

    Allow respawn after lose screen

--- scenes/utils/SceneChanger.gd
+++ scenes/utils/SceneChanger.gd
@@ -5,4 +5,6 @@ extends Area2D
 
 func _on_SceneChanger_body_entered(body):
 	if body.get_name() == "Player":
+		if self.get_name() == "RocketGoal" and scene_name.begins_with("Level"):
+			Global.current_level = scene_name
 		get_tree().change_scene_to_file(str("res://scenes/" + scene_name + ".tscn"))
